f634c40461a20a3104013f818a84818b59989c22
chomskim <csknova@gmail.com>
2021-01-17 00:09:14 +0900

    add hero image to blog

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { graphql, Link } from "gatsby";
+import HeroImage from "gatsby-image";
 import Layout from "../components/Layout";
-import styles from "./blog-list.module.css";
+import styles from "./blog.module.css";
 
-export default function BlogTemplate({ data, pageContext }) {
+export default function BlogTemplate({ data }) {
   // Generate the previous and next page URLs.
+  const title = data.markdownRemark.frontmatter.title;
+  const html = data.markdownRemark.html;
+  const heroImage = data.markdownRemark.frontmatter.heroImage;
+
   return (
     <Layout>
+      {heroImage && (
+        <HeroImage className={styles.hero} fluid={heroImage.childImageSharp.fluid} />
+      )}
       <div className={styles.blog}>
-        <h1>{data.markdownRemark.frontmatter.title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
+        <h1>{title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: html }} />
       </div>
     </Layout>
   );
@@ -20,6 +28,13 @@ export const query = graphql`
       html
       frontmatter {
         title
+        heroImage {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
       }
     }
   }
